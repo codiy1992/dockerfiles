72d20e254d63ebd6dacbc733728f52ec907a63ae
codiy <mail@codiy.net>
2022-12-18 12:57:48 +0800

    fix

--- cray/v2ray.sh
+++ cray/v2ray.sh
@@ -3,17 +3,19 @@
 ACME_DIR=${ACME_DIR:-'acme-v02.api.letsencrypt.org-directory'}
 
 ACME_CERT_FILE=/data/caddy/certificates/${ACME_DIR}/${DOMAIN}/${DOMAIN}.crt
-if [[ "${DOMAIN}" != "" && -f /etc/cray/v2ray.json && -f ${ACME_CERT_FILE} ]]; then
+ACME_CERT_FILE_CDN=/data/caddy/certificates/${ACME_DIR}/${DOMAIN_CDN}/${DOMAIN_CDN}.crt
+
+if [[ -f /etc/cray/v2ray.json ]] && [[ -f ${ACME_CERT_FILE} || -f ${ACME_CERT_FILE_CDN} ]]; then
     cp /etc/cray/v2ray.json /etc/v2ray/config.json
     sed -i "s/ACME_DIR/${ACME_DIR}/g" /etc/v2ray/config.json
-    sed -i "s/DOMAIN/${DOMAIN}/g" /etc/v2ray/config.json
 fi
 
-ACME_CERT_FILE_CDN=/data/caddy/certificates/${ACME_DIR}/${DOMAIN_CDN}/${DOMAIN_CDN}.crt
-if [[ "${DOMAIN_CDN}" != "" && -f /etc/cray/v2ray.json && -f ${ACME_CERT_FILE_CDN} ]]; then
-    cp /etc/cray/v2ray.json /etc/v2ray/config.json
-    sed -i "s/ACME_DIR/${ACME_DIR}/g" /etc/v2ray/config.json
+if [[ -f ${ACME_CERT_FILE_CDN} ]]; then
     sed -i "s/DOMAIN_CDN/${DOMAIN_CDN}/g" /etc/v2ray/config.json
+fi
+
+if [[ -f ${ACME_CERT_FILE} ]]; then
+    sed -i "s/DOMAIN/${DOMAIN}/g" /etc/v2ray/config.json
 fi
 
 /usr/bin/v2ray run -c /etc/v2ray/config.json
--- cray/xray.sh
+++ cray/xray.sh
@@ -3,17 +3,19 @@
 ACME_DIR=${ACME_DIR:-'acme-v02.api.letsencrypt.org-directory'}
 
 ACME_CERT_FILE=/data/caddy/certificates/${ACME_DIR}/${DOMAIN}/${DOMAIN}.crt
-if [[ "${DOMAIN}" != "" && -f /etc/cray/xray.json && -f ${ACME_CERT_FILE} ]]; then
+ACME_CERT_FILE_CDN=/data/caddy/certificates/${ACME_DIR}/${DOMAIN_CDN}/${DOMAIN_CDN}.crt
+
+if [[ -f /etc/cray/xray.json ]] && [[ -f ${ACME_CERT_FILE} || -f ${ACME_CERT_FILE_CDN} ]]; then
     cp /etc/cray/xray.json /etc/xray/config.json
     sed -i "s/ACME_DIR/${ACME_DIR}/g" /etc/xray/config.json
-    sed -i "s/DOMAIN/${DOMAIN}/g" /etc/xray/config.json
 fi
 
-ACME_CERT_FILE_CDN=/data/caddy/certificates/${ACME_DIR}/${DOMAIN_CDN}/${DOMAIN_CDN}.crt
-if [[ "${DOMAIN_CDN}" != "" && -f /etc/cray/xray.json && -f ${ACME_CERT_FILE_CDN} ]]; then
-    cp /etc/cray/xray.json /etc/xray/config.json
-    sed -i "s/ACME_DIR/${ACME_DIR}/g" /etc/xray/config.json
+if [[ -f ${ACME_CERT_FILE_CDN} ]]; then
     sed -i "s/DOMAIN_CDN/${DOMAIN_CDN}/g" /etc/xray/config.json
+fi
+
+if [[ -f ${ACME_CERT_FILE} ]]; then
+    sed -i "s/DOMAIN/${DOMAIN}/g" /etc/xray/config.json
 fi
 
 /usr/bin/xray run -c /etc/xray/config.json
